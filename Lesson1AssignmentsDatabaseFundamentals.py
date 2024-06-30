7a1739ee6a268ad70e4d035935f74148df8808b3
jameszhang01194320 <wuab@yahoo.com>
2024-06-30 11:13:48 -0700

    week5day2Assignment

---
+++ Lesson1AssignmentsDatabaseFundamentals.py
@@ -0,0 +1,30 @@
+# Lesson 1: Assignments | Database Fundamentals
+# Remember to take your time and work through each question diligently! Test your code, make sure it works, and try to find ways to improve. Once you are happy and satisfied with your code, upload it to Github, then turn in your Github link at the bottom of the page!
+
+# Don't forget. Make sure this assignment is in it's own repository. Not mixed in with others!
+
+# 1. Designing and Implementing a Relational Database for a Bookstore
+
+
+# Objective: The aim of this assignment is to apply the concepts of relational database design, normalization, and entity-relationship modeling to create an efficient and structured database for a bookstore management system, "BookHaven."
+
+
+
+# Problem Statement: "BookHaven" is facing challenges in managing its extensive collection of books, author information, customer data, and transaction records. The current system has issues with data redundancy, inconsistency, and inefficient data retrieval. Your task is to design a relational database that addresses these problems while ensuring data integrity and ease of access.
+
+# Design a normalized database ERD for "BookHaven". Which should be able to handle the following.
+# A Customer can make many Transactions
+# A Transaction can include many Books 
+# A Book can be sold many times
+# A Book is only written by one Author
+# An Author can write many books
+
+
+# Task 1: Entity-Relationship Diagram (ERD) Creation
+
+# Develop an ERD to visually represent the relevant data and datatypes that would be stored in the the tables Customer, Transactions, Books, Author .
+# The cardinality should accurately represent the relationships such as the link between Books and Authors,  Customers and Transactions. Transactions and Books (hint: this is a many-to-many relationship)
+
+
+# Expected Outcome: A clear and detailed ERD that accurately reflects the relationships within the "BookHaven" database.
+
